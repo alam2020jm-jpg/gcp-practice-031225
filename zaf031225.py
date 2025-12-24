6935728f935ca49ccc29060647b20ce90f47b0ea
zaffir <alam2020jm@gmail.com>
2025-12-24 14:24:53 +0530

    work completed and ready for commit and push

--- zaf031225.py
+++ zaf031225.py
@@ -4,3 +4,8 @@ c="your name is dastageer"
 d="your name is  salehin"
 e="your name Sahil"
 
+a1=int(input("enter the first number"))
+b1=int(input("enter the second number"))
+c1=a1+b1
+print(f"Sum of  {a1} and  {b1} will be {c}")
+
